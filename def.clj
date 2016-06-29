266c85d839637aa59a7eed3f079fd2bb0f08c10c
Sridhar <sreedharriyer@gmail.com>
2016-06-28 21:55:33 -0700

    def

---
+++ def.clj
@@ -0,0 +1,8 @@
+
+(def var "var is a placeholder, def is used to bind name to value")
+; prints "var is a placeholder, def is used to bind name to value"
+var
+
+(def number 1000)
+; prints 1000
+number

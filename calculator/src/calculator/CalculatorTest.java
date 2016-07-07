3ab96dc529801a41c377c259b7066c8038b33316
Sridhar <sreedharriyer@gmail.com>
2016-07-06 17:34:30 -0700

    adding java test file

---
+++ calculator/src/calculator/CalculatorTest.java
@@ -0,0 +1,8 @@
+public class CalculatorTest {
+	public static void main(String[] args) {
+	  Calculator calculator = new Calculator();
+	  System.out.println(calculator.sayHello("Sridhar"));
+	  System.out.println(calculator.add(5,3));
+	  System.out.println(calculator.multiply(5,3));
+	}
+}
